2844670322077c7c6aeb382c46752797eaab19de
Andrey Bakurskii <58373565+AndreyBakurskii@users.noreply.github.com>
2021-04-09 12:05:36 +0300

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -1,14 +1,39 @@
-#include <iostream>
 #include <stdio.h>
-#include "task1.h"
+#include <iostream>
 
 using namespace std;
 
+int getPrime(int n) {
+	
+	int chislo = 1;
+	int is_complex = 0;
+	int i = 0;
+
+
+	while (i < n)
+	{
+		is_complex = 0;
+		chislo++;
+
+		for (int delitel = 2; delitel < chislo; delitel++) {
+
+			if (chislo % delitel == 0) {
+				is_complex++;
+				break;
+			}
+		}
+
+		if (is_complex == 0) {
+			i++;
+		}
+	}
+	return chislo;
+	
+}
+
 int main()
 {
-	int n;
-	cout << "Enter a number: ";
-	cin >> n;
+	int n = 19;
 	int answer = getPrime(n);
 	cout << answer << endl;
 	return 0;
